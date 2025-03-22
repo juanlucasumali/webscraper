f7278bf761f6a659ba5271c780a9fa4b340b9c86
Juan Lucas Umali <85742113+juanlucasumali@users.noreply.github.com>
2025-03-22 04:03:04 +0000

    Try not headless

--- webscraper.py
+++ webscraper.py
@@ -53,7 +53,7 @@ class AirbnbScraper:
     def setup_driver(self):
         """Set up the Chrome driver with appropriate options"""
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Run in headless mode
+        # chrome_options.add_argument("--headless")  # Run in headless mode
         # chrome_options.add_argument("--headless=new")  # Run in headless mode
         # chrome_options.add_argument("--no-sandbox")
         # chrome_options.add_argument("--disable-dev-shm-usage")
